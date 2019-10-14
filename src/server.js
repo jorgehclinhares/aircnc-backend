7019cc0057105a5113245b2f265ca164fdd3b53e
Jorge Linhares <jorgehclinhares@gmail.com>
2019-10-14 13:27:29 -0300

    refactor: servindo arquivos estáticos

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const path = require('path')
 const app = express()
 const routes = require('./routes')
 
@@ -12,5 +13,6 @@ mongoose.connect('mongodb+srv://jorgehclinhares:coruja123@aircnc-n6qqq.mongodb.n
 })
 
 app.use(cors())
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(routes)
 app.listen(8000)
